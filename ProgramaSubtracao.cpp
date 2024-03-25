e0dc601610963d9adf3730d4357b013e2b9339e1
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-03-25 23:01:59 +0000

    feito

---
+++ ProgramaSubtracao.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    int n1,n2;
+    
+    
+    printf("digite o primeiro numero para fazer a soma: ");
+    scanf("%d", &n1);
+    printf("digite o primeiro numero para fazer a soma: ");
+    scanf("%d", &n2);
+    int soma = n1 + n2;
+    printf("Soma: %d + %d = %d\n", n1 ,n2, soma);
+
+    return 0;
+
+}
